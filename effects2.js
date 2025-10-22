26757e941bea7d8214227babf0533f0603b5982d
skidinghub <235431650+skidinghub@users.noreply.github.com>
2025-10-22 17:52:23 -0500

    Update effects2.js

--- effects2.js
+++ effects2.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/1430655091584667660?theme=dark&bg=00000&idleMessage=probly%20banned";
+    const imgSrc = "https://lanyard.cnrad.dev/api/1430655091584667660?theme=dark&bg=00000&idleMessage=probly%20banned&showDisplayName=true&hideActivity=true";
     const imgElement = document.getElementById('rpc');
     imgElement.src = imgSrc;
 });
+
 
 
