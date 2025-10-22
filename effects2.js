b72ec1b2187ff3e68d0b241089e3acffc74e559f
skidinghub <235431650+skidinghub@users.noreply.github.com>
2025-10-22 17:45:57 -0500

    Update image source URL with idle message

--- effects2.js
+++ effects2.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgSrc = "https://lanyard.cnrad.dev/api/1430655091584667660?theme=dark&bg=00000";
+    const imgSrc = "https://lanyard.cnrad.dev/api/1430655091584667660?theme=dark&bg=00000&idleMessage=probly%20banned";
     const imgElement = document.getElementById('rpc');
     imgElement.src = imgSrc;
 });
+
 
